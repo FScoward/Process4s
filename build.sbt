10b04f003462f4d7ff485ca1ea37154e37642e53
FScoward <FScoward@FSCOWARD-PC>
2015-01-29 23:46:53 +0900

    sl4j + logback でログ出力するように修正

--- build.sbt
+++ build.sbt
@@ -5,5 +5,10 @@ version := "1.0"
 scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
-  "org.eclipse.swt.win32.win32" % "x86" % "3.3.0-v3346"
+  "org.eclipse.swt.win32.win32" % "x86" % "3.3.0-v3346",
+  "joda-time" % "joda-time" % "2.3",
+  "org.joda" % "joda-convert" % "1.6",
+  "org.slf4j" % "slf4j-api" % "1.7.10",
+  "ch.qos.logback" % "logback-classic" % "1.1.2",
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,12 @@
-import java.io.{InputStreamReader, BufferedReader}
+import java.io.{PrintWriter, InputStreamReader, BufferedReader}
+
+import org.joda.time.format.ISODateTimeFormat
+import org.slf4j.LoggerFactory
 
 import scala.collection.{mutable, Iterator}
 import org.eclipse.swt.internal.win32.{TCHAR, OS}
+import scala.concurrent.duration.{DurationInt, DurationLong}
+import com.typesafe.scalalogging.{LazyLogging, Logger}
 
 /**
  * Created by FScoward on 2015/01/27.
@@ -9,16 +14,19 @@ import org.eclipse.swt.internal.win32.{TCHAR, OS}
 object Main {
   def main(args: Array[String]): Unit = {
     
-    val pl = getProcessList
-    val x = pl.drop(3)
-    
-    val list = WindowInfo.toWindowInfoList(x)
-    list.foreach(println)
-    
     // アクティブウィンドウを取得し、何を作業記録を行う
+    val LOGGER = LoggerFactory.getLogger(this.getClass)
+    val term = 5
+    
+    while(true){
+      
+      LOGGER.debug(s"${getForegroundWindowTitle}")
+
+      Thread.sleep((term seconds).toMillis)
+    }
 
   }
- 
+  
   /**
    * 最前面ウィンドウのタイトルを取得する
    * */
